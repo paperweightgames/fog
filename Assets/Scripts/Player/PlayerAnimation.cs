036811cd2bbbb020927ef58f34e615a4b2b019f5
Justinas Grigas <jstn_as@protonmail.com>
2021-03-17 18:16:21 +0000

    Added player animations - Added animations - Added player animator - Added a player animation script

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rb;
+        private Animator _animator;
+        private int _animation = Idle;
+        private static readonly int Idle = Animator.StringToHash("Idle");
+        private static readonly int Run = Animator.StringToHash("Run");
+        private static readonly int Jump = Animator.StringToHash("Jump");
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            var velocity = _rb.velocity;
+            if (Mathf.Abs(velocity.y) > .1)
+                SetAnimation(Jump);
+            else if (Mathf.Abs(velocity.x) + Mathf.Abs(velocity.z) > .1)
+                SetAnimation(Run);
+            else
+                SetAnimation(Idle);
+        }
+
+        private void SetAnimation(int anim)
+        {
+            if (anim == _animation)
+                return;
+            _animator.SetBool(_animation, false);
+            _animation = anim;
+            _animator.SetBool(_animation, true);
+        }
+    }
+}
